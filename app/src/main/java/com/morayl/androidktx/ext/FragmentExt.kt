656a91d0994f701b16e0aec4e9ebbd55998d5c67
morayl <sea.gang.moray@gmail.com>
2021-03-25 13:06:27 +0900

    Navigation componentのExtを追加

--- app/src/main/java/com/morayl/androidktx/ext/FragmentExt.kt
+++ app/src/main/java/com/morayl/androidktx/ext/FragmentExt.kt
@@ -1,8 +1,16 @@
 package com.morayl.androidktx.ext
 
+import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.annotation.MainThread
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavArgs
+import androidx.navigation.NavArgsLazy
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.findNavController
 import kotlin.properties.ReadOnlyProperty
 
 /**
@@ -37,3 +45,87 @@ inline fun <reified VM : ViewModel> Fragment.getViewModel(crossinline factory: (
  */
 inline fun <reified VM : ViewModel> Fragment.viewModelProvider(crossinline factory: () -> VM) =
     lazy { getViewModel { factory() } }
+
+fun Fragment.backOrFinishActivity() {
+    if (!popBackStack()) {
+        activity?.finish()
+    }
+}
+
+@MainThread
+inline fun <reified Args : NavArgs> Fragment.nullableAavArgs() = NavArgsLazy(Args::class) {
+    arguments ?: Bundle()
+}
+
+fun Fragment.navigate(directions: NavDirections) {
+    findNavController().navigate(directions)
+}
+
+fun Fragment.navigate(@IdRes id: Int) {
+    findNavController().navigate(id)
+}
+
+fun Fragment.navigate(directions: NavDirections, navigatorExtras: Navigator.Extras) {
+    findNavController().navigate(directions, navigatorExtras)
+}
+
+fun Fragment.popBackStack(): Boolean = findNavController().popBackStack()
+
+const val DEFAULT_NAVIGATION_RESULT_KEY = "result"
+
+inline fun <reified T> Fragment.observeNavResult(
+    key: String = T::class.simpleName ?: DEFAULT_NAVIGATION_RESULT_KEY,
+    clearWhenObserve: Boolean = true,
+    crossinline onChanged: (T) -> Unit
+) {
+    val savedStateHandle = findNavController().currentBackStackEntry?.savedStateHandle
+    savedStateHandle?.getLiveData<T>(key)?.observe(viewLifecycleOwner) { t ->
+        t ?: return@observe
+        if (clearWhenObserve) {
+            savedStateHandle.set(key, null)
+        }
+        onChanged(t)
+    }
+}
+
+inline fun <reified T> Fragment.setNavResult(result: T, key: String = T::class.simpleName ?: DEFAULT_NAVIGATION_RESULT_KEY) {
+    findNavController().previousBackStackEntry?.savedStateHandle?.set(key, result)
+}
+
+inline fun <reified T> Fragment.setNavResultToCurrent(result: T, key: String = T::class.simpleName ?: DEFAULT_NAVIGATION_RESULT_KEY) {
+    findNavController().currentBackStackEntry?.savedStateHandle?.set(key, result)
+}
+
+inline fun <reified T> Fragment.setNavResult(
+    result: T,
+    @IdRes target: Int,
+    key: String = T::class.simpleName ?: DEFAULT_NAVIGATION_RESULT_KEY
+) {
+    findNavController().getBackStackEntry(target).savedStateHandle.set(key, result)
+}
+
+inline fun <reified T> Fragment.setNavResultAndPopUp(result: T, key: String = T::class.simpleName ?: DEFAULT_NAVIGATION_RESULT_KEY) {
+    setNavResult(result, key)
+    findNavController().popBackStack()
+}
+
+/**
+ * popBackStackしてからnavResultをセットする
+ * 主にDialogから値をセットする際に利用する
+ */
+inline fun <reified T> Fragment.popUpAndSetNavResult(result: T, key: String = T::class.simpleName ?: DEFAULT_NAVIGATION_RESULT_KEY) {
+    findNavController().popBackStack()
+    viewLifecycleOwner.doOnDestroy {
+        setNavResultToCurrent(result, key)
+    }
+}
+
+inline fun <reified T> Fragment.setNavResultAndPopUp(
+    result: T,
+    @IdRes destination: Int,
+    key: String = T::class.simpleName ?: DEFAULT_NAVIGATION_RESULT_KEY,
+    inclusive: Boolean = false
+) {
+    setNavResult(result, destination, key)
+    findNavController().popBackStack(destination, inclusive)
+}
--- app/src/main/java/com/morayl/androidktx/ext/LifecycleExt.kt
+++ app/src/main/java/com/morayl/androidktx/ext/LifecycleExt.kt
@@ -1,5 +1,6 @@
 package com.morayl.androidktx.ext
 
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
@@ -11,6 +12,18 @@ fun LifecycleOwner.doOnResume(action: () -> Unit) {
         fun doOnResume() {
             lifecycle.removeObserver(this)
             action.invoke()
+        }
+    })
+}
+
+/**
+ * ライフサイクル終了時の処理となるため、副作用が懸念されるような時間のかかる処理等は注意してください
+ */
+fun LifecycleOwner.doOnDestroy(action: () -> Unit) {
+    lifecycle.addObserver(object : DefaultLifecycleObserver {
+        override fun onDestroy(owner: LifecycleOwner) {
+            owner.lifecycle.removeObserver(this)
+            action()
         }
     })
 }
