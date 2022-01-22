0ca7d7ef51bd7c603117cf013358f047a01d4c42
morayl <sea.gang.moray@gmail.com>
2022-01-22 16:34:07 +0900

    [add] 各種Sourceを追加

---
+++ app/src/main/java/com/morayl/androidktx/parameter/ColorSource.kt
@@ -0,0 +1,43 @@
+package com.morayl.androidktx.parameter
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Parcelable
+import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import kotlinx.android.parcel.Parcelize
+
+sealed class ColorSource : Parcelable {
+    @ColorInt
+    abstract fun getColor(context: Context): Int
+
+    fun getColorDrawable(context: Context): ColorDrawable = ColorDrawable(getColor(context))
+
+    @Parcelize
+    data class Text(private val colorText: String, private val defaultColor: ColorSource) : ColorSource() {
+        override fun getColor(context: Context): Int = runCatching { Color.parseColor(colorText) }
+            .getOrElse {
+                defaultColor.getColor(context)
+            }
+    }
+
+    @Parcelize
+    data class Resource(@ColorRes private val colorRes: Int) : ColorSource() {
+        override fun getColor(context: Context): Int = ContextCompat.getColor(context, colorRes)
+    }
+
+    /**
+     * themeによって色が変わります
+     * getColorで渡されたcontextにセットされているthemeが適用されます
+     */
+    @Parcelize
+    data class Attribute(@AttrRes private val colorAttrRes: Int) : ColorSource() {
+        override fun getColor(context: Context): Int = TypedValue().apply {
+            context.theme.resolveAttribute(colorAttrRes, this, true)
+        }.data
+    }
+}
---
+++ app/src/main/java/com/morayl/androidktx/parameter/DrawableSource.kt
@@ -0,0 +1,34 @@
+package com.morayl.androidktx.parameter
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Parcelable
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
+import kotlinx.android.parcel.Parcelize
+
+sealed class DrawableSource : Parcelable {
+
+    abstract fun getDrawable(context: Context): Drawable?
+
+    @Parcelize
+    data class Resource(@DrawableRes val resId: Int) : DrawableSource() {
+        override fun getDrawable(context: Context): Drawable? {
+            return AppCompatResources.getDrawable(context, resId)
+        }
+    }
+
+    @Parcelize
+    data class ResourceWithTint(@DrawableRes val resId: Int, val tintColor: ColorSource) : DrawableSource() {
+        override fun getDrawable(context: Context): Drawable? {
+            return AppCompatResources.getDrawable(context, resId)?.mutate()?.apply {
+                setTint(tintColor.getColor(context))
+            }
+        }
+    }
+
+    companion object {
+        operator fun invoke(@DrawableRes resId: Int) = Resource(resId)
+        operator fun invoke(@DrawableRes resId: Int, tintColor: ColorSource) = ResourceWithTint(resId, tintColor)
+    }
+}
