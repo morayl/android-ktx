04a521bf84f7a7dccbd877e49781790e7e19ff2a
morayl <sea.gang.moray@gmail.com>
2022-01-23 23:22:02 +0900

    [add] ViewModel.launchを追加

---
+++ app/src/main/java/com/morayl/androidktx/ext/ViewModelExt.kt
@@ -0,0 +1,18 @@
+package com.morayl.androidktx.ext
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+fun ViewModel.launch(
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+): Job {
+    return viewModelScope.launch(context, start, block)
+}
