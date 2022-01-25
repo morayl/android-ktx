84f4a79a20d148db3f6fb567ea549a4f5d56bbbe
morayl <sea.gang.moray@gmail.com>
2022-01-25 22:35:21 +0900

    [modify] deprecatedをDefaultLifecycleに変更

--- app/src/main/java/com/morayl/androidktx/ext/LifecycleExt.kt
+++ app/src/main/java/com/morayl/androidktx/ext/LifecycleExt.kt
@@ -1,17 +1,13 @@
 package com.morayl.androidktx.ext
 
 import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.OnLifecycleEvent
 
 fun LifecycleOwner.doOnResume(action: () -> Unit) {
-    lifecycle.addObserver(object : LifecycleObserver {
-        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-        fun doOnResume() {
-            lifecycle.removeObserver(this)
-            action.invoke()
+    lifecycle.addObserver(object : DefaultLifecycleObserver {
+        override fun onResume(owner: LifecycleOwner) {
+            owner.lifecycle.removeObserver(this)
+            action()
         }
     })
 }
