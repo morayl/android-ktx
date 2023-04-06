7cf66a8e08a5dbb3d824defe0ecfc518db9f2ac4
morayl <sea.gang.moray@gmail.com>
2023-04-06 16:11:42 +0900

    EnumMapに特定のenum値だけを入れられるようにする

--- app/src/main/java/com/morayl/androidktx/toplevel/EnumMap.kt
+++ app/src/main/java/com/morayl/androidktx/toplevel/EnumMap.kt
@@ -3,5 +3,10 @@ package com.morayl.androidktx.toplevel
 import java.util.EnumMap
 
 @Suppress("FunctionNaming")
-inline fun <reified Key : Enum<Key>, Value> EnumMap(valueBlock: (Key) -> Value): EnumMap<Key, Value> =
-    enumValues<Key>().associateWithTo(EnumMap(Key::class.java), valueBlock)
+inline fun <reified Key : Enum<Key>, Value> EnumMap(entries: List<Key>? = null, valueBlock: (Key) -> Value): EnumMap<Key, Value> {
+    return if (entries != null) {
+        enumValues<Key>().filter { entries.contains(it) }.associateWithTo(EnumMap(Key::class.java), valueBlock)
+    } else {
+        enumValues<Key>().associateWithTo(EnumMap(Key::class.java), valueBlock)
+    }
+}
